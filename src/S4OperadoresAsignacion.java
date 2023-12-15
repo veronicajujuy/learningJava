62eef78b2daaeabf03bfda5a1b7d4e2c35cc752e
veronicajujuy <veronica.jujuy@gmail.com>
2023-12-14 22:26:40 -0300

    actualizando S4

---
+++ src/S4OperadoresAsignacion.java
@@ -0,0 +1,20 @@
+public class S4OperadoresAsignacion {
+    public static void main(String[] args) {
+        // asignacion
+        int j=1, i=4;
+
+        i+=5; // es i = i + 5
+        System.out.println("i = " + i);
+        i-=1; // es i = i -1
+        System.out.println("i = " + i);
+        i/=j+1; // es i = i/(j+1)
+        System.out.println("i = " + i);
+        
+        String unString = "Hola ";
+        unString += "soy un hermoso ";
+        unString += "String xD";
+        System.out.println("unString = " + unString);
+
+
+    }
+}
