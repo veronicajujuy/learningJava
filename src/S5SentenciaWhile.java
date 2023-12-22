368ace8f2a8ed5a2f24fca2f10cdba675799feb3
veronicajujuy <veronica.jujuy@gmail.com>
2023-12-22 13:24:15 -0300

    agregando seccion 5

---
+++ src/S5SentenciaWhile.java
@@ -0,0 +1,25 @@
+public class S5SentenciaWhile {
+    public static void main(String[] args) {
+        int i=0;
+        while (i<5){
+            System.out.println("i = " + i);
+            i++;
+        }
+
+        boolean prueba = true;
+        i=0;
+        while(prueba){
+            if(i == 7){
+                prueba = false;
+            }
+            System.out.println("i = " + i);
+            i++;
+        }
+        // do while
+        prueba = false;
+
+        do{
+            System.out.println("esto se ejecuta una vez");
+        }while (prueba);
+    }
+}
---
+++ src/S5Tarea9.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class S5Tarea9 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ingrese 2 numeros enteros positivos o negativos");
+        int num1 = s.nextInt();
+        int num2 = s.nextInt();
+        boolean signo = false, signoGeneral = false;
+        
+        int multiplicacion = 0;
+        if (num1 <0 && num2 <0){
+            signoGeneral = true;
+        }
+        if (num1 < 0) {
+            num1 = -num1;
+            signo = true;
+        }
+        if (num2 < 0) {
+            num2= -num2;
+            signo = true;
+        }
+
+        for(int i = 1; i<=num1;i++){
+            multiplicacion +=num2;
+        }
+        if (signo){
+            multiplicacion = -(multiplicacion);
+        }
+        if (signoGeneral){
+            multiplicacion = -(multiplicacion);
+        }
+        System.out.println("multiplicacion = " + multiplicacion);
+    }
+}
